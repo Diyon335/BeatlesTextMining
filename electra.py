1487e4f155eb57002f161a576851a79f17f16378
Diyon335 <diyonreal@gmail.com>
2023-05-24 13:18:48 +0200

    added more evaluation metrics

--- electra.py
+++ electra.py
@@ -9,6 +9,7 @@ import random
 import numpy as np
 import evaluate
 import wandb
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 data_folder = "data/pre_processed_data/sentences/all/"
@@ -147,10 +148,20 @@ def fine_tune():
 
 def compute_metrics(eval_pred):
 
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
+    logits, labels = eval_pred.predictions, eval_pred.label_ids
+    predictions = np.argmax(logits, axis=1)
 
-    return metric.compute(predictions=predictions, references=labels)
+    accuracy = accuracy_score(labels, predictions)
+    precision = precision_score(labels, predictions, average='weighted')
+    recall = recall_score(labels, predictions, average='weighted')
+    f1 = f1_score(labels, predictions, average='weighted')
+
+    return {
+        'accuracy': accuracy,
+        'precision': precision,
+        'recall': recall,
+        'f1': f1
+    }
 
 
 def plot_custer_diagram(title, song_dict, output_path):
